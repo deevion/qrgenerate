86d20430cdb1ab3ba6a645d7a0e605868cdc85d4
deevmath <matheus.httsp@gmail.com>
2023-06-21 22:20:08 -0300

    Adicionado button de download, do QRCODE

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const button = document.querySelector('button'),
-  img = document.querySelector('img'),
-  input = document.querySelector('input'),
-  api_1 = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=`
+const button = document.getElementById('generate')
+const download = document.getElementById('download')
+const img = document.querySelector('img')
+const input = document.querySelector('input')
+const api_1 = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=`
 
 button.addEventListener('click', () => {
   img.src = `${api_1}${input.value}`
+})
+
+download.addEventListener('click', async () => {
+  const response = await fetch(img.src)
+  const blod = await response.blob()
+  const downloadLink = document.createElement('a')
+  downloadLink.href = URL.createObjectURL(blod)
+  downloadLink.download = 'qrcode.jpg'
+  downloadLink.click()
 })
